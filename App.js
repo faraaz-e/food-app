136cd940e02358cd8e5828044c8b198566b00e38
faraaz-e <efaraaz111@gmail.com>
2023-07-06 15:23:46 +0530

    Added React functionalities
    Includes - start/build scripts, React Element, JSX, Babel, Components, Fragments

--- App.js
+++ App.js
@@ -1,25 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-/**
- * Nested elements
- * 
- * <div id="parent">
- *    <div id="child">
- *      <h1> This is h1 tag </h1>
- *      <h2> This is h1 tag </h2>
- *    </div>
- * </div>
- * 
- */
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is h1 tag"),
-    React.createElement("h2", {}, "This is h2 tag"),
-  ])
+// React Element
+// React.createElement (object) => renders => HTMLElement
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello World!");
+console.log(heading); // it is an object not an html element
+
+// JSX (transpiled before it reaches the JS via Parcel - Babel[js compiler - converts jsx to react BTS])
+// JSX => React.createElement => results Object => rendered => HTMLElement
+
+const jsxHeading = (
+  <h1 id="jsxHead" className="customheading">
+    Hello JSX
+  </h1>
+);
+console.log(jsxHeading); // this is an object not an html element
+
+// React Functional Component
+
+const TestComponent = () => {
+  return <h1>Hello Test Functional Component</h1>;
+};
+
+const HeadingComponent = () => (
+  <div id="container">
+    {jsxHeading}
+    {TestComponent()}
+    <TestComponent />
+    <TestComponent></TestComponent>
+    <h1 className="heading"> Hello Functional Component </h1>
+  </div>
 );
 
+/**
+ * first letter of variable - uppercase
+ * { } used to include normal javascript
+ * < /> used to include functional components
+ */
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// root.render(jsxHeading); // after rendering an object it becomes html element on webpage
+
+root.render(<HeadingComponent />); // way to render functional components (using tags </>)
--- README.md
+++ README.md
@@ -42,3 +42,45 @@
 
 * It is package that enables our application to be compatible with the older version of browsers
 * To configure it, add it in package.json (refer the documentation / browserslist.dev)
+
+#### Ep - 03 : Laying the foundation
+
+* Adding scripts to _package.json_ to avoid using `npx parcel index.html` to start the server.
+  ```JSON
+  "scripts": {
+    "start": "parcel index.html", /** to start the server (localhost:1234) */
+    "build": "parcel build index.html", /** production build of project */
+    ...
+  },
+  ```
+* After adding the above, now run the app using `npm run start` or `npm start`
+
+> React Element
+  `const test = React.createElement("h1", {id: "demo"}, "Hello World!")` results an object and not a html element. It converts to HTML element after rendering i.e. `root.render(test)`
+
+> JSX
+  It is not HTML in Javascript, it looks like HTML syntax. JSX is basically a combination of HTML & Javascript.
+
+> Babel
+
+  Babel is a transpiler / js-compiler which converts JSX to React behind the scenes.
+
+> React Components
+
+  * Class based components (old method)
+  * Function based components (new method)
+
+> React Fragments
+
+  As JSX allows only one parent in component, so we need to wrap it with `<div></div>` to add more than one parent element, example,
+  ```HTML
+  <div>
+    <div class="parent">
+      <h1 class="child"></h1>
+    </div>
+    <div class="parent">
+      <h2 class="child"></h2>
+    </div>
+  </div>
+  ``` 
+  To avoid using `<div>...</div>` (which looks inappropriate in html structure) instead, we can use `<React.Fragment>...</React.Fragment>` or an empty tag `<>...</>`
